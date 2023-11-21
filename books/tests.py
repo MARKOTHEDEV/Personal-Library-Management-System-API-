55b7bea3202361093dbee2ae8cc8e8bb809bc8bc
Nwokolo Matthew <markothedevmail@gmail.com>
2023-11-21 21:56:08 +0100

    added test for authentication and authorization rules.~

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from rest_framework.test import APITestCase
 # Create your tests here.
+
+
+class TestSetUp(APITestCase):
+    
+    
+    def setUp(self) -> None:
+        
+        return super().setUp()
