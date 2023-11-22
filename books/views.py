0ab841cc73a5b1e10eca73da853725238e166795
Nwokolo Matthew <markothedevmail@gmail.com>
2023-11-22 08:51:44 +0100

    added rate limit to the book management api

--- books/views.py
+++ books/views.py
@@ -2,16 +2,12 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from . import serializer,models,permissions
 from utils.pagination import LargeResultsSetPagination
-from drf_yasg.utils import swagger_auto_schema
 from django.utils.decorators import method_decorator
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from django_ratelimit.decorators import ratelimit
 
 
-
-# @method_decorator(name='list', decorator=swagger_auto_schema(
-#     operation_description="where users manage thier book",
-#     security=[{"Bearer": []}],
-# ))
+@method_decorator(ratelimit(key='ip', rate='12/m', method='GET'), name='list')
 class UserManageBooksViewset(viewsets.ModelViewSet):
     serializer_class = serializer.UserManageBooksSerializer
     queryset = models.Book.objects.all()
